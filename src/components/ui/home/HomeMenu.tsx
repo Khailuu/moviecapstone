2cc93a3e0bf56ab00b715d350bcf0a651ea51df0
Khải Luu <khailuu@Khais-MacBook-Pro.local>
2024-05-12 01:06:55 +0700

    up

--- src/components/ui/home/HomeMenu.tsx
+++ src/components/ui/home/HomeMenu.tsx
@@ -1,35 +1,98 @@
 import { useState } from "react";
 import { Tabs } from "antd";
 import { useGetLichChieuHeThongRap } from "hooks/api/useGetLichChieuHeThongRap";
-
-type TabPosition = 'left' | 'right' | 'top' | 'bottom';
+import "../../../assets/custom.css";
+import moment from "moment";
+type TabPosition = "left" | "right" | "top" | "bottom";
 
 export const HomeMenu = () => {
-    const { data: rapList } = useGetLichChieuHeThongRap()
-    console.log(rapList)
-    const [tabPosition] = useState<TabPosition>('left');
+  const { data: rapList } = useGetLichChieuHeThongRap();
+  console.log(rapList);
+  const [tabPosition] = useState<TabPosition>("left");
   return (
     <div>
-            <>
+      <>
+        <Tabs tabPosition={tabPosition}>
+          {rapList?.map((rap, i) => {
+            const id = String(i + 1);
+            return (
+              <Tabs.TabPane
+                tab={
+                  <div>
+                    <img
+                      src={rap.logo}
+                      alt={rap.tenHeThongRap}
+                      style={{ width: "60px", marginRight: "8px" }}
+                    />
+                  </div>
+                }
+                key={id}
+              >
                 <Tabs tabPosition={tabPosition}>
-                    {rapList?.map((rap, i) => {
-                        const id = String(i + 1);
-                        return (
-                            <Tabs.TabPane
-                                tab={
-                                    <div>
-                                        <img src={rap.logo} alt={rap.tenHeThongRap} style={{ width: '60px', marginRight: '8px' }} />
-                                        
-                                    </div>
-                                }
-                                key={id}
-                            >
-                                {`Content of Tab ${id}`}
-                            </Tabs.TabPane>
-                        );
-                    })}
+                  {rap.lstCumRap?.map((cumRap, i) => {
+                    const id = String(i + 1);
+                    return (
+                      <Tabs.TabPane
+                        tab={
+                          <div style={{ width: 250, display: "flex" }}>
+                            <img
+                              src="https://movie-booking-project.vercel.app/img/cumRap/lotte-cinema-cong-hoa-15383860949090.jpg"
+                              alt="logo"
+                              style={{ width: 50 }}
+                            />
+                            <p className="text-white mt-3 ml-[15px] fw-bold">
+                              {cumRap.tenCumRap}
+                            </p>
+                          </div>
+                        }
+                        key={id}
+                      >
+                        {cumRap.danhSachPhim?.map((phim) => {
+                          return (
+                            <div style={{ display: "flex" }}>
+                              <img
+                                src={phim.hinhAnh}
+                                style={{
+                                  width: 120,
+                                  height: 120,
+                                  marginBottom: 30,
+                                }}
+                                alt="logophim"
+                              />
+                              <div className=" ml-[15px]">
+                                <h2
+                                  className="text-[#6bb140]"
+                                  style={{ fontSize: 24, fontWeight: "bold" }}
+                                >
+                                  {phim.tenPhim}
+                                </h2>
+                                <p className="text-white">{cumRap.diaChi}</p>
+                                <div className="grid grid-cols-6">
+                                  {phim.lstLichChieuTheoPhim?.slice(0, 10).map(
+                                    (lichChieu: any) => {
+                                      return (
+                                        <h2 className="text-[#86de4f] text-[20px] mr-[15px]">
+                                          {moment(
+                                            lichChieu.ngayChieuGioChieu
+                                          ).format("HH:mm")}
+                                        </h2>
+                                      );
+                                    }
+                                  )}
+                                </div>
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </Tabs.TabPane>
+                    );
+                  })}
                 </Tabs>
-            </>
-        </div>
+              </Tabs.TabPane>
+            );
+          })}
+        </Tabs>
+      </>
+    </div>
   );
 };
--- src/types/QuanLyRap.ts
+++ src/types/QuanLyRap.ts
@@ -1,7 +1,25 @@
 export type Rap = {
   logo: string;
-  lstCumRap: [];
+  lstCumRap: [LstCumRap];
   maHeThongRap: string;
   manhom: string;
   tenHeThongRap: string;
 };
+export type LstCumRap = {
+  danhSachPhim: any[];
+  diaChi: string;
+  hinhAnh: string;
+  maCumRap: string;
+  tenCumRap: string;
+};
+
+export type phim = {
+  dangChieu: boolean;
+  hinhAnh: string;
+  hot: boolean;
+  lstLichChieuTheoPhim: any[];
+  maPhim: string;
+  sapChieu: boolean;
+  tenPhim: string;
+};
+
